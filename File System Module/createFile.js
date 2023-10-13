59587917ab1063e44277fbf9429dadeb89a9694d
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2023-10-13 23:35:18 +0600

    Update createFile.js

--- File System Module/createFile.js
+++ File System Module/createFile.js
@@ -1,6 +1,11 @@
+var http = require('http');
 var fs = require('fs');
 
+var server = http.createServer(function (req, res) {
+  
 fs.appendFile('mynewfile.txt', 'Hello ..from new File!', function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
+});
+server.listen(5500);

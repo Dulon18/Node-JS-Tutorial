73105957e56e06cdd7e011fd9781c01a8009ff21
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2023-10-16 23:57:45 +0600

    Update updateFile.js

--- File System Module/updateFile.js
+++ File System Module/updateFile.js
@@ -3,7 +3,7 @@ var http = require('http');
 var fs = require('fs');
 var server = http.createServer(function (req, res) {
   
-fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
+fs.appendFile('demo.txt', ' This is my text.', function (err) {
   if (err) throw err;
   console.log('Updated!');
 });

490dddafdbb311ce5266f75f00b34403cff0cffd
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2023-10-19 22:54:14 +0600

    Create event.js

---
+++ Events/event.js
@@ -0,0 +1,10 @@
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var eventHandler = function () {
+  console.log('I hear a scream!');
+}
+//Assign the eventhandler to an event:
+eventEmitter.on('scream', eventHandler);
+eventEmitter.emit('scream');

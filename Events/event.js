9461525425e0dc8e0e90b6f2bc8968bd9d71d7ae
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2023-11-10 23:59:58 +0600

    Update event.js

--- Events/event.js
+++ Events/event.js
@@ -6,6 +6,8 @@ var eventHandler = function () {
   console.log('I hear a scream!');
 }
 //Assign the eventhandler to an event:
+
+
 eventEmitter.on('scream', eventHandler);
 eventEmitter.emit('scream');
 

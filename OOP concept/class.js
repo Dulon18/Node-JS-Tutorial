02387e82c279326e5ba0c7216c8694c6f579d13a
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2024-03-01 22:14:40 +0600

    Create class.js

---
+++ OOP concept/class.js
@@ -0,0 +1,7 @@
+class Person {
+    constructor (name, address,) {
+        this.name = name
+        this.phrase = address
+    }
+    get_info = () => console.log(`I'm  ${this.name}.I'm from ${this.address}`)
+}
